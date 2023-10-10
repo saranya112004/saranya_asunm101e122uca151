4a8ffd6f1dd369ecc3d7479dacdd9b62f3fa8e14
saranya112004 <147485264+saranya112004@users.noreply.github.com>
2023-10-10 13:13:40 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,16 @@
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+num = int(input("Enter a number: "))
+
+if num < 0:
+  print("Factorial cannot be calculated for negative numbers.")
+elif num == 0:
+  print("Factorial of 0 is 1.")
+else:
+  result = factorial(num)
+  print("Factorial of", num, "is", result)
